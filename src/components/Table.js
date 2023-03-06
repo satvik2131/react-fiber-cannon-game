9f8523080b8be41bcae9ec52c4f2e5f47f9cca31
Satvik Kushwaha <59243339+satvik2131@users.noreply.github.com>
2023-03-06 21:21:07 +0530

    declaring properties forlooo
    Signed-off-by: Satvik Kushwaha <59243339+satvik2131@users.noreply.github.com>

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,18 +4,16 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 
 export function Table() {
-    const [ref] = usePlane(() => ({
-        mass: 1,
+    const [ref, tableApi] = usePlane(() => ({
+        mass: 10,
         position: [0, 0, 0],
-        rotation: [0, 4.78, 0],
+        rotation: [0, -Math.PI / 2, 0],
         type: "Static"
     }));
 
-
-
     const gltf = useLoader(GLTFLoader, './wooden_table/scene.gltf')
     return (
-        <mesh >
+        <mesh update={tableApi}>
             <primitive ref={ref} object={gltf.scene} scale={1.0} />
             <ambientLight intensity={0.5} />
             <directionalLight color="green" />
