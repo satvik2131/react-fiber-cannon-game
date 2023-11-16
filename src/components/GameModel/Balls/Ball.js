51aee77e481deb6541ec90139c6783574c6189cd
satvik2131 <satvik213161@gmail.com>
2023-11-16 19:06:12 +0530

    tr

--- src/components/GameModel/Balls/Ball.js
+++ src/components/GameModel/Balls/Ball.js
@@ -11,7 +11,7 @@ export function Ball({ position }) {
     type: "Dynamic",
     position: position,
     args: [0.18, 0.18, 0.18],
-    // sleepSpeedLimit: 1,
+    // sleepSpeedLimit: 0,
   }));
 
   useFrame(() => {});
